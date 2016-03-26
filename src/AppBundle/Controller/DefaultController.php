66f103e2ee11d2c8b28eab435db4f8bf2c934aa6
barik94 <basya.den@mail.ru>
2016-03-26 12:26:09 +0200

    Homepage view

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,15 +11,19 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function renderOfficersInfoAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $em = $this->container->get('doctrine')->getEntityManager();
+
+        $officers = $em->getRepository('AppBundle:Officer')->findAll();
+
+        return $this->render('homepage.html.twig', array(
+            'officers' => $officers,
+        ));
     }
 
     /**
